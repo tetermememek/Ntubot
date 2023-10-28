8b9a85313f5a652b8f3a2785d0b08911ba4f6bb2
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-10-28 22:23:08 +0700

    update modules/_help.py

--- modules/_help.py
+++ modules/_help.py
@@ -41,60 +41,14 @@ async def _help(ayra):
                         x += "\n"
                     x += "\n© @KynanSupport"
                     await ayra.eor(x)
-                except BaseException:
-                    file = None
-                    compare_strings = []
-                    for file_name in LIST:
-                        compare_strings.append(file_name)
-                        value = LIST[file_name]
-                        for j in value:
-                            j = cmd_regex_replace(j)
-                            compare_strings.append(j)
-                            if j.strip() == plug:
-                                file = file_name
-                                break
-                    if not file:
-                        # the enter command/plugin name is not found
-                        text = f"`{plug}` is not a valid plugin!"
-                        if best_match := next(
-                            (
-                                _
-                                for _ in compare_strings
-                                if plug in _ and not _.startswith("_")
-                            ),
-                            None,
-                        ):
-                            text += f"\nDid you mean `{best_match}`?"
-                        return await ayra.eor(text)
-                    output = f"**Perintah** `{plug}` **ditemukan dalam** - `{file}`\n"
-                    if file in HELP["Official"]:
-                        for i in HELP["Official"][file]:
-                            output += i
-                    output += "\n© @KynanSupport"
-                    await ayra.eor(output)
+                except BaseException as e:
+                    await ayra.eor(f"{e}")
         except BaseException as er:
             LOGS.exception(er)
             await ayra.eor("Error 🤔 occured.")
     else:
         try:
             results = await ayra.client.inline_query(asst.me.username, "ayra")
-        except BotMethodInvalidError:
-            z = []
-            for x in LIST.values():
-                z.extend(x)
-            cmd = len(z) + 10
-            return await ayra.reply(
-                get_string("inline_4").format(
-                    OWNER_NAME,
-                    len(HELP["Official"]),
-                    cmd,
-                ),
-                buttons=_main_help_menu,
-            )
-        except BotResponseTimeoutError:
-            return await ayra.eor(
-                get_string("help_2").format(HNDLR),
-            )
         except BotInlineDisabledError:
             return await ayra.eor(get_string("help_3"))
         await results[0].click(chat.id, reply_to=ayra.reply_to_msg_id, hide_via=True)
