7d739628473a3da2b307e4cbd4cf632f39197fef
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-11-05 11:26:22 +0700

    update modules/_help.py

--- modules/_help.py
+++ modules/_help.py
@@ -18,7 +18,7 @@ _main_help_menu = [
 ]
 
 
-@ayra_cmd(pattern="[hH][eE][lL][pP]( (.*)|$)")
+@ayra_cmd(pattern="^[Hh][Ee][Ll][Pp]( (.*)|$)")
 async def _help(ayra):
     plug = ayra.pattern_match.group(1).strip()
     chat = await ayra.get_chat()
