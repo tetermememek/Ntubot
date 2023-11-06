ba8f2bdc987c70466c7cf8c4e515e8c8779bba7c
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-11-07 01:03:25 +0700

    update modules/lock.py

--- modules/lock.py
+++ modules/lock.py
@@ -20,26 +20,22 @@ from . import ayra_cmd
 
 
 @ayra_cmd(
-    pattern="[Ll][Oo][Cc][Kk]|[Uu][Nn][Ll][Oo][Cc][Kk]( (.*)|$)",
+    pattern="(un|)lock( (.*)|$)",
     admins_only=True,
     manager=True,
     require="change_info",
 )
-async def lock_unlock(e):
-    pler = e.pattern_match.group(2)
-    if not pler:
+async def un_lock(e):
+    mat = e.pattern_match.group(2).strip()
+    if not mat:
         return await e.eor("`Berikan kata kunci yang tepat..`", time=5)
-
-    lock = e.pattern_match.group(0).lower() == ""
-
-    ml = lock_unlock(pler, lock)
+    lock = e.pattern_match.group(1) == ""
+    ml = lock_unlock(mat, lock)
     if not ml:
         return await e.eor("`Salah`", time=5)
-
     msg = "Locked" if lock else "Unlocked"
     try:
         await e.client(EditChatDefaultBannedRightsRequest(e.chat_id, ml))
     except Exception as er:
         return await e.eor(str(er))
-
-    await e.eor(f"**{msg}** - `{pler}` ! ")
+    await e.eor(f"**{msg}** - `{mat}` ! ")
