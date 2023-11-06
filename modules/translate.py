d94bda9661070618875c35c3f306850da3c397bf
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-11-07 01:19:40 +0700

    update modules/translate.py

--- modules/translate.py
+++ modules/translate.py
@@ -27,7 +27,7 @@ async def lu_pro(jink):
     trans = Translator()
     b = 'id'
     if jink.is_reply:
-        teks = jink.reply_to.text or jink.reply_to.caption
+        teks = jink.reply.text or jink.reply.caption
         if not teks:
             return await jink.reply("Tidak ada teks yang dapat dideteksi.")
         hasil = await trans.detect(teks)
