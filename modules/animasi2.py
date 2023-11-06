d34fcaaa334c895d4fc9e99464788ad6a7ee7fb9
naya1503 <naya1503@users.noreply.github.com>
2023-11-06 03:57:18 +0000

    Auto Fix

--- modules/animasi2.py
+++ modules/animasi2.py
@@ -78,9 +78,7 @@ async def _(event):
 
 @ayra_bot.on(events.NewMessage(pattern=r"\.$(.*)", outgoing=True))
 async def _(event):
-
     if event.fwd_from:
-
         return
 
     animation_interval = 3
@@ -121,9 +119,7 @@ async def _(event):
 
 @ayra_bot.on(events.NewMessage(pattern=r"\.$(.*)", outgoing=True))
 async def _(event):
-
     if event.fwd_from:
-
         return
 
     animation_interval = 1
@@ -163,9 +159,7 @@ async def _(ayiin):
 
 @bot.on(events.NewMessage(pattern=r"\.$(.*)", outgoing=True))
 async def _(event):
-
     if event.fwd_from:
-
         return
 
     animation_interval = 0.5
