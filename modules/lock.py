73bef3ec6ed7358f189db1da6e164627f9d0f1d8
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-11-07 00:32:17 +0700

    update modules/lock.py

--- modules/lock.py
+++ modules/lock.py
@@ -20,7 +20,7 @@ from . import ayra_cmd
 
 
 @ayra_cmd(
-    pattern="(un|)lock( (.*)|$)", admins_only=True, manager=True, require="change_info"
+    pattern="([Uu][nN|)[lL]ock( (.*)|$)", admins_only=True, manager=True, require="change_info"
 )
 async def un_lock(e):
     mat = e.pattern_match.group(2).strip()
