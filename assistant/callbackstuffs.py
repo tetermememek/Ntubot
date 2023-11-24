97154cd41b6620fb0f732bf15c6c88bf4987dd3c
mymasky <144117937+mymasky@users.noreply.github.com>
2023-11-24 13:47:30 +0700

    Update callbackstuffs.py

--- assistant/callbackstuffs.py
+++ assistant/callbackstuffs.py
@@ -166,7 +166,7 @@ TOKEN_FILE = "resources/auths/auth_token.txt"
 )
 async def send(eve):
     key, name = (eve.data_match.group(1)).decode("UTF-8").split("_")
-    thumb = "https://graph.org/file/60408fea8439e6702674d.jpg"
+    thumb = "https://telegra.ph//file/cb6f5bb9793c135d78afd.jpg"
     await eve.answer("■ Sending ■")
     data = f"uh_{key}_"
     index = None
@@ -254,7 +254,7 @@ async def changes(okk):
     )
     cli = "\n\nKlik tombol di bawah untuk memperbarui!"
     if not match:
-        thumb = "https://graph.org/file/60408fea8439e6702674d.jpg"
+        thumb = "https://telegra.ph//file/cb6f5bb9793c135d78afd.jpg"
         try:
             if len(tl_chnglog) > 700:
                 tl_chnglog = f"{tl_chnglog[:700]}..."
